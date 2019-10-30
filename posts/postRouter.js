fb49b18b7411e751b39cffc229ed1f0c0f221a19
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-10-30 18:17:10 +0100

    postRouter done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,84 @@
-const express = 'express';
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const dbPost = require("./postDb");
 
+router.use(express.json());
+
+router.get("/", (req, res) => {
+  dbPost
+    .get()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Server error: " + error.message
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", validatePostId, (req, res) => {
+  dbPost
+    .getById(req.post.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Server error: " + error.message
+      });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete("/:id", validatePostId, (req, res) => {
+  dbPost
+    .remove(req.post.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Server error: " + error.message
+      });
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validatePostId, (req, res) => {
 
+    dbPost
+    .update(req.body)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+        console.log(error)
+      res.status(200).json({
+        errorMessage: "Server error " + error.message
+      });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
-};
+  const id = req.params.id;
+  dbPost
+    .getById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res.status(404).json({ message: "invalid post id" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Server error: " + error.message
+      });
+    });
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const server = express();
 const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter");
 
 server.use(express.json());
 server.use(logger);
 
 server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
